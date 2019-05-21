195706e0f47c82d839d812cbb5bbcadf887da850
thomas lombardo <tomlom1984@hotmail.co.uk>
2019-05-21 19:38:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,6 +22,9 @@ var funkyFunction = function() {
     return "FUNKY!";
   };
 };
+function theFunk () {
+  return "FUNKY";
+}
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
